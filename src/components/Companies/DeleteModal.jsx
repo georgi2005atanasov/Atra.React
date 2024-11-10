dc08bff9386459c17dc62e069334c79ad07b9bc5
georgi2005atanasov <gogida9876@abv.bg>
2024-11-10 23:53:45 +0200

    made companies modal for delete also responsive

--- src/components/Companies/DeleteModal.jsx
+++ src/components/Companies/DeleteModal.jsx
@@ -1,50 +1,48 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 const DeleteModal = ({ handleDelete, setDeleteModal, deleteModal }) => {
-    return ReactDOM.createPortal(<>
-      <div className="modal-backdrop fade show w-100" />
-      <div className="modal d-flex-justify-content-center align-items-center" tabIndex="-1">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Потвърждение за изтриване</h5>
-              <button
-                type="button"
-                className="btn-close"
-                onClick={() =>
-                  setDeleteModal({ isOpen: false, companyName: "" })
-                }
-              />
+    return ReactDOM.createPortal(
+        <>
+            <div className="modal-backdrop fade show w-100" />
+            <div className="modal d-flex justify-content-center align-items-center" tabIndex="-1">
+                <div className="modal-dialog modal-dialog-centered modal-sm modal-md modal-lg">
+                    <div className="modal-content p-3">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Потвърждение за изтриване</h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                onClick={() => setDeleteModal({ isOpen: false, companyName: "" })}
+                            />
+                        </div>
+                        <div className="modal-body">
+                            <p className="mb-0">
+                                Сигурен ли сте че искате да изтриете &apos;
+                                {deleteModal.companyName}&apos;?
+                            </p>
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                onClick={() => setDeleteModal({ isOpen: false, companyName: "" })}
+                            >
+                                Отказ
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-danger"
+                                onClick={() => handleDelete(deleteModal.companyName)}
+                            >
+                                Изтрий
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="modal-body">
-              <p className="mb-0">
-                Сигурен ли сте че искате да изтриете &apos;
-                {deleteModal.companyName}&apos;?
-              </p>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() =>
-                  setDeleteModal({ isOpen: false, companyName: "" })
-                }
-              >
-                Отказ
-              </button>
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => handleDelete(deleteModal.companyName)}
-              >
-                Изтрий
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>, document.getElementById('root')
-  );
+        </>,
+        document.getElementById('root')
+    );
 };
 
 export default DeleteModal;
